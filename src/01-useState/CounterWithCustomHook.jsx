44868cf7ee395510260807abfba42cc70ef2614e
Alejandro2097 <alejo12381@hotmail.es>
2023-05-24 21:44:43 -0500

    improvment

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -7,9 +7,9 @@ export const CounterWithCustomHook = () => {
     <>
         <div>Counter With hook: {counter}</div>
         <hr/>
-        <button className="btn btn-primary" onClick={() =>  increment()}>+1</button>
-        <button className="btn btn-primary" onClick={() => reset()}>Reset</button>
-        <button className="btn btn-primary" onClick={() => decrement()}>-1</button>
+        <button className="btn btn-primary" onClick={() =>  increment(2)}>+1</button>
+        <button className="btn btn-primary" onClick={reset}>Reset</button>
+        <button className="btn btn-primary" onClick={() => decrement(2)}>-1</button>
     </>
   )
 }
