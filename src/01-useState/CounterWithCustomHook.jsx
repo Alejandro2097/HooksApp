69f168843317d400b60c1f63a41ec7ec53d00758
Alejandro2097 <alejo12381@hotmail.es>
2023-05-24 21:33:41 -0500

    my solution to the homework

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,13 +1,13 @@
 import { useCounter } from "../hooks/useCounter"
 
 export const CounterWithCustomHook = () => {
-    const {counter} = useCounter()
+    const {counter, increment} = useCounter()
 
   return (
     <>
         <div>Counter With hook: {counter}</div>
         <hr/>
-        <button className="btn btn-primary">+1</button>
+        <button className="btn btn-primary" onClick={() =>  increment()}>+1</button>
         <button className="btn btn-primary">Reset</button>
         <button className="btn btn-primary">-1</button>
     </>
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,9 +1,24 @@
 import { useState } from "react"
 
 export const useCounter = (initialValue = 10) => {
-    const [counter, setCounter] = useState(initialValue)
+    const [counter, setCounter] = useState(initialValue);
+
+    const increment = () =>  {
+        increment
+    }
+
+    const decrement = () => {
+        setCounter(initialValue);
+    }
+
+    const reset = () => {
+        setCounter( counter - 1);
+    }
 
     return {
         counter,
+        increment,
+        decrement,
+        reset
     }
 }
