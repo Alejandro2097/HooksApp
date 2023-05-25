03547500cbf461e4262fbcce6e283c011d08ebbd
Alejandro2097 <alejo12381@hotmail.es>
2023-05-24 21:48:41 -0500

    start with useEffect

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,21 @@
+
+export const SimpleForm = () => {
+  return (
+    <>
+        <h1>Formulario simple</h1>
+        <hr/>
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+        />
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+        />
+    </>
+  )
+}
