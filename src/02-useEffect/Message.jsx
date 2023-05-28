cffe006bd6ca9186d1fdcbe294086fe948c139d8
Alejandro2097 <alejo12381@hotmail.es>
2023-05-28 18:45:35 -0500

    useEffect more depply

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react"
 
 export const Message = () => {
+  useEffect(() => {
+    const onMouseMove = ({x, y}) =>  {
+      const cords = {x, y};
+      console.log(cords);
+    }
+    window.addEventListener('mousemove', onMouseMove)
+  
+    return () => {
+    }
+  }, [])
+  
   return (
     <h3>Usuario ya existe</h3>
   )
