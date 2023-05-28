6bddc9ddc978eb2e71a18f390a74c3e079435ddd
Alejandro2097 <alejo12381@hotmail.es>
2023-05-28 18:55:39 -0500

    new class

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
+  const [coords] = useState({x: 0, y:0});
   useEffect(() => {
     const onMouseMove = ({x, y}) =>  {
       const cords = {x, y};
@@ -9,10 +10,15 @@ export const Message = () => {
     window.addEventListener('mousemove', onMouseMove)
   
     return () => {
+      window.removeEventListener('mousemove', onMouseMove)
     }
   }, [])
   
   return (
-    <h3>Usuario ya existe</h3>
+    <>
+        <h3>Usuario ya existe</h3>
+        { JSON.stringify(coords)}
+    </>
+    
   )
 }
