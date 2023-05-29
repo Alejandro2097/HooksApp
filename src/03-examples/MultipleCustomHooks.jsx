4fa608f5ed0e209d9a9a39a303024a7d647f686f
Alejandro2097 <alejo12381@hotmail.es>
2023-05-28 22:14:09 -0500

    curls

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,5 +1,9 @@
+import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
+
+    const {data, isLoading, hasError} = useFetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    console.log({data, isLoading, hasError})
   return (
     <>
         <h1>BreakingBad Quotes</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </React.StrictMode>,
 )
