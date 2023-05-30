c364d07fe1cc320cbca230c042ee775d7b573a9e
Alejandro2097 <alejo12381@hotmail.es>
2023-05-29 20:56:03 -0500

    solucion tarea

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,13 +1,31 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
 
-    const {data, isLoading, hasError} = useFetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    const { counter , increment  } = useCounter();
+    const {data, isLoading, hasError} = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
     console.log({data, isLoading, hasError})
   return (
     <>
         <h1>BreakingBad Quotes</h1>
         <hr/>
+         {
+          isLoading
+            ? (
+              <div className="alert alert-info text-center">
+                Loading...
+              </div>
+            ) : (
+              <blockquote className="blockquote text-end">
+                <p className="mb-1">{data.base_experience}</p>
+                <footer className="blockquote-footer">{data.name}</footer>
+              </blockquote>
+            )
+         }
+         <button className="btn btn-primary" onClick={() => increment()}>
+          Next quote
+         </button>
     </>
   )
 }
