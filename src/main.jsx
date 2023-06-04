15e2f062039b27e6935745152c360c7c11c35f9a
Alejandro2097 <alejo12381@hotmail.es>
2023-06-04 18:50:39 -0500

    styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './index.css'
 import { Layout } from './05-useLayoutEffect/Layout'
+
+import './index.css'
 
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
