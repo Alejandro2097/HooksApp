15d1258a16620cfa430cda8db0226729715b7d62
Alejandro2097 <alejo12381@hotmail.es>
2023-06-04 18:49:35 -0500

    Layout

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,30 @@
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "../03-examples";
+
+export const Layout = () => {
+
+    const { counter , increment  } = useCounter();
+    const {data, isLoading, hasError} = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
+    console.log({data, isLoading, hasError})
+  return (
+    <>
+        <h1>BreakingBad Quotes</h1>
+        <hr/>
+         {
+          isLoading
+            ? (
+              <LoadingQuote/>
+            ) : (
+              <Quote quote={data.id} name={data.name}/>
+            )
+         }
+         <button 
+                className="btn btn-primary" 
+                disabled={isLoading}
+                onClick={() => increment()}
+                >
+          Next quote
+         </button>
+    </>
+  )
+}
