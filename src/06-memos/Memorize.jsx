eab17fd9ced9ca921f022f19c85a4c7ff760d026
Alejandro2097 <alejo12381@hotmail.es>
2023-06-04 19:21:59 -0500

    se re dibuja mas de lo esperado

--- src/06-memos/Memorize.jsx
+++ src/06-memos/Memorize.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { useCounter } from "../hooks"
+import { Small } from "./Small";
 
 export const Memorize = () => {
     const {counter, increment} = useCounter(10);
+    const [show, setShow] = useState(true);
   return (
     <>
-        <h1>Counter: <small>{counter}</small></h1>
+        <h1>Counter: <Small value={counter}/></h1>
         <hr/>
 
         <button
@@ -12,6 +15,12 @@ export const Memorize = () => {
             onClick={() => increment()} 
         >
             +1
+        </button>
+        <button
+            className="btn btn-outline-primary"
+            onClick={() => setShow(!show)}
+        >
+            Show/Hide { JSON.stringify(show) }
         </button>
     </>
   )
