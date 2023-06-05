51bc103c26ee3db624ab34b606daeee5933253dd
Alejandro2097 <alejo12381@hotmail.es>
2023-06-04 19:08:37 -0500

    memorize

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export const Quote = ({quote, name}) => {
+
+  const pRef = useRef();
+  const [boxSize, setBoxSize] = useState({ width: 0, height: 0});
+  useLayoutEffect(() => {
+    const {height, width} = pRef.current.getBoundingClientRect();
+    setBoxSize({height, width});
+  }, [quote])
   return (
     <>
-        <blockquote className="blockquote text-end">
-            <p className="mb-1">{quote}</p>
+        <blockquote 
+          className="blockquote text-end"
+          style={{display: 'flex'}}
+        >
+            <p ref={pRef} className="mb-1">{quote}</p>
             <footer className="blockquote-footer">{name}</footer>
         </blockquote>
+        <code>{JSON.stringify(boxSize)}</code>
     </>
   )
  
