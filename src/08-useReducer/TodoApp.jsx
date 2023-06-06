fc21eb28862f5db92581b7e43fe228eb5f2d4694
Alejandro2097 <alejo12381@hotmail.es>
2023-06-05 22:28:41 -0500

    reducer implementation

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,21 @@
+import { useReducer } from "react";
+
+const initialState = [
+    {
+        id:  new Date().getTime(),
+        description: 'Recolectar la pedra del alma',
+        done: false
+    },
+    {
+        id:  new Date().getTime() * 3,
+        description: 'Recolectar la pedra del alma',
+        done: false
+    },
+];
 
 export const TodoApp = () => {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
         <h1>TodoApp</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 
 // import { Padre } from './07-tarea-memo/Padre'
@@ -21,7 +21,7 @@ import './index.css'
 import { TodoApp } from './08-useReducer/TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <TodoApp />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
