2f712b90c628934e568a79ff7da93b07e42c01ec
Alejandro2097 <alejo12381@hotmail.es>
2023-06-05 21:50:34 -0500

    Intro reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// import { Layout } from './05-useLayoutEffect/Layout'
-
-import './index.css'
 import { Padre } from './07-tarea-memo/Padre'
+
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Memorize } from './06-memos/Memorize'
@@ -16,6 +14,10 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import  './08-useReducer/intro-reducer';
+// import { Layout } from './05-useLayoutEffect/Layout'
+
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
