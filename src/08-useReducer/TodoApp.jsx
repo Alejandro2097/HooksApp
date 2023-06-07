979a5658be4c27d0611cd61e9b1a66774892707d
Alejandro2097 <alejo12381@hotmail.es>
2023-06-06 21:44:34 -0500

    inicio de la tarea

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -17,28 +17,35 @@ const initialState = [
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState)
+    const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+    const handleNewTodo = (todo) => {
+
+    }
   return (
     <>
         <h1>TodoApp (10), <small>pendientes: 2</small></h1>
         <hr/>
         <div className="row">
             <div className="col-7">
+                {/* Todo likst */}
                 <ul className="list-group">
                     {
+                        // Todo item
                         todos.map(todo => {
-                            
+                            <li key={todo.id} className="list-group-item d-flex justify-content-between">
+                                <span className="align-self-center"> Item 1</span>
+                                <button className="btn btn-danger">Borrar</button>
+                            </li>
                         })
                     }
-                    <li className="list-group-item d-flex justify-content-between">
-                       <span className="align-self-center"> Item 1</span>
-                       <button className="btn btn-danger">Borrar</button>
-                    </li>
                 </ul>
             </div>
             <div className="col-5">
                 <h4>Agregar TODO</h4>
                 <hr/>
+                {/* TodoAdd onNewTodo(todo) */}
+                {/* { id: new Date()..., description: '', done: false} */}
                 <form>
                     <input
                         type="text"
