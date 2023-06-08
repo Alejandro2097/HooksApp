d6d817f11bf30458059c45c9865db2db2cb9d82d
Alejandro2097 <alejo12381@hotmail.es>
2023-06-07 21:39:15 -0500

    iniciando la tarea de fernando fuamnos xdxd

--- src/08-useReducer/TodoItem.jsx
+++ src/08-useReducer/TodoItem.jsx
@@ -4,7 +4,7 @@ export const TodoItem = ({todo, onDeleteTodo, onToggleTodo}) => {
     <>
         <li  className="list-group-item d-flex justify-content-between">
             <span 
-              className="align-self-center"
+              className={`align-self-center ${todo.done ? 'text-decoration-line-through' : ''}`}
               onClick={() => onToggleTodo(todo.id)}
             >
               { todo.description}
