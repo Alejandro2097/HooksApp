08b3f8acdd2d0dfd21e31069a13867a135eb7804
Alejandro2097 <alejo12381@hotmail.es>
2023-06-07 21:08:30 -0500

    handle delete todo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -35,6 +35,12 @@ export const TodoApp = () => {
         }
         dispatch(action);
     }
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
   return (
     <>
         <h1>TodoApp (10), <small>pendientes: 2</small></h1>
@@ -42,7 +48,7 @@ export const TodoApp = () => {
         <div className="row">
             <div className="col-7">
                 {/* Todo likst */}
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onDeleteTodo={id => onNewTodo}/>
             </div>
             <div className="col-5">
                 <h4>Agregar TODO</h4>
