e3b123da6018fbbd1945aa28a0e4cec506bc7350
Alejandro2097 <alejo12381@hotmail.es>
2023-06-07 21:44:32 -0500

    solucionamos la tarea de fernando eso hpta xd

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,52 +1,13 @@
-import { useEffect, useReducer } from "react";
-
-import { todoReducer } from "./todoREducer";
 import { TodoList } from "./TodoList";
 import { TodoAdd } from "./TodoAdd";
+import { useTodo } from "./useTodo";
 
-const initialState = [
-    {
-        id:  new Date().getTime(),
-        description: 'Recolectar la pedra del alma',
-        done: false
-    },
-    {
-        id:  new Date().getTime() * 3,
-        description: 'Recolectar la pedra del tiempo',
-        done: false
-    },
-];
+
 
 export const TodoApp = () => {
 
-    const init = () => {
-        return JSON.parse(localStorage.getItem('todos') || []);
-    }
-
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-    useEffect(() => {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos])
-    
-    const handleNewTodo = (todo) => {
-        const action = { 
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
-        dispatch(action);
-    }
-    const handleDeleteTodo = (id) => {
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        })
-    }
-    const handleToggleTodo = (id) => {
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        })
-    }
+   
+    const {todos , handleNewTodo, handleDeleteTodo, handleToggleTodo} =  useTodo()
   return (
     <>
         <h1>TodoApp (10), <small>pendientes: 2</small></h1>
--- src/08-useReducer/useTodo.js
+++ src/08-useReducer/useTodo.js
@@ -1,7 +1,54 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "./todoREducer";
 
 
 export const useTodo = () => {
-  return (
-    <div>useTodo</div>
-  )
+    const initialState = [
+        {
+            id:  new Date().getTime(),
+            description: 'Recolectar la pedra del alma',
+            done: false
+        },
+        {
+            id:  new Date().getTime() * 3,
+            description: 'Recolectar la pedra del tiempo',
+            done: false
+        },
+    ];
+
+    const init = () => {
+        return JSON.parse(localStorage.getItem('todos') || []);
+    }
+    
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+    
+    const handleNewTodo = (todo) => {
+        const action = { 
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action);
+    }
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo
+  }
 }
