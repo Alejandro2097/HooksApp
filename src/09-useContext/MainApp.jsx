5608c33db8ec376db38309f3eb03e33be25956f6
Alejandro2097 <alejo12381@hotmail.es>
2023-06-11 18:25:17 -0500

    first route

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import { HomePage } from './HomePage'
 
 export const MainApp = () => {
   return (
     <>
         <h1>MainApp</h1>
         <hr/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
     </>
   )
 }
