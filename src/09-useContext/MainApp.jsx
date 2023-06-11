721509c74bcf6a5af104d12a159c014f5a735d47
Alejandro2097 <alejo12381@hotmail.es>
2023-06-11 18:36:21 -0500

    anchor tags

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -8,7 +8,11 @@ export const MainApp = () => {
   return (
     <>
         <h1>MainApp</h1>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/login">Login</a>
         <hr/>
+        
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="about" element={<AboutPage/>}/>
