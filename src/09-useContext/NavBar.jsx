c97c973b1fb784b3c6bfe35fcc441e67c9602bc6
Alejandro2097 <alejo12381@hotmail.es>
2023-06-11 20:07:52 -0500

    seco

--- src/09-useContext/NavBar.jsx
+++ src/09-useContext/NavBar.jsx
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export const NavBar = () => {
   return (
-    <>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/login">Login</Link>
-    </>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">useContext</Link>
+          
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                    <NavLink
+                        className={(args) => {
+                            console.log(args);
+                            return 'nav-link'
+                        }} 
+                        to="/">
+                        Home
+                    </NavLink>
+                    <NavLink
+                        className={(args) => {
+                            console.log(args);
+                            return 'nav-link'
+                        }} 
+                        to="/about">
+                        About
+                    </NavLink>
+                </ul>
+    
+            </div>
+        </div>
+    </nav>
   )
 }
